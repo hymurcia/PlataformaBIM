b4df0f6fadbf1f3426824ca43c3cee6b4e5b5896
Hernan Yessid Murcia Salinas <112666763+hymurcia@users.noreply.github.com>
2025-11-25 21:10:44 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  client_encoding: 'UTF8' 
+  client_encoding: 'UTF8',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
